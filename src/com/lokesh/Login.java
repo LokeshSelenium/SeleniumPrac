97ec573bef9d4b43b72964d1cd8e502633520fe6
Lokesh Sharma <lokesh.sharma@nttdata.com>
2018-05-28 12:39:52 +0530

    login changes

---
+++ src/com/lokesh/Login.java
@@ -0,0 +1,19 @@
+package com.lokesh;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Login {
+
+	public static void main(String[] args)
+	{
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://localhost/login.do");
+		
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+		    
+	}
+
+}
